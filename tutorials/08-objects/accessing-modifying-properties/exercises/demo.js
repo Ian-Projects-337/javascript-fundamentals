de61180ccdc132b7212877ac73dc2a0c6df49537
Ian <Ian.f.becker@gmail.com>
2026-05-22 11:47:22 -0500

    Finalized demo

--- tutorials/08-objects/accessing-modifying-properties/exercises/demo.js
+++ tutorials/08-objects/accessing-modifying-properties/exercises/demo.js
@@ -9,16 +9,17 @@ const cargoBay = {
 };
 
 // DEMO: Use dot notation to modify the tonnage
-
+cargoBay.tonnage = 145;
 // DEMO: Use dot notation to add a technician property
-
+cargoBay.Technician = "Amos";
 // DEMO: Use dot notation to form a sentence using properties of the object
 // and log it to the console.
-
+console.log(cargoBay);
+console.log(`${cargoBay.manifestId} holds ${cargoBay.tonnage}t of cargo`)
 // DEMO: Use bracket notation to update the containment status
-
+cargoBay["containment-status"] = "VENTING";
 // DEMO: Use bracket notation to print an alert about the containment status
-
+console.log(`Alert Status: ${cargoBay["containment-status"]}`);
 /** EXAMPLE 2 - DYNAMIC VARIABLE LOOKUPS **/
 
 const engineCore = {
@@ -30,9 +31,9 @@ const engineCore = {
 let statRequested = "pressure";
 
 // DEMO: What happens if we try to use dot notation here?
-
+console.log(engineCore.statRequested);
 // DEMO: Use the dynamic key to get the stat
-
+console.log(engineCore[statRequested]);
 // DEMO: Make a git commit!
 
 /* 
