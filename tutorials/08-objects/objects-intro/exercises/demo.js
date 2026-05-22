9f7a67a535859f6553691a2164e01b832ca0e056
Ian <Ian.f.becker@gmail.com>
2026-05-22 11:32:34 -0500

    Finished demo

--- tutorials/08-objects/objects-intro/exercises/demo.js
+++ tutorials/08-objects/objects-intro/exercises/demo.js
@@ -3,18 +3,23 @@
 /** EXAMPLE 1 - DECLARATION & INITIALIZATION **/
 
 // DEMO: Create a structured profile for a ship using Key-Value pairs
-
+const ShipProfile = {
+  name: "Rocinante",
+  classType: "Corvette-class Gunship",
+  crewCapacity: 4,
+  isDriveActive: false
+};
 // DEMO: Print the object to the console
-
+console.log(ShipProfile);
 /** EXAMPLE 2 - THE MEMORY REFERENCE PARADOX **/
 
 const sectorA_Scanner = { oxygenLevel: 98 };
 const sectorB_Scanner = { oxygenLevel: 98 };
 
 // DEMO: Test if the two objects are equal
-
+console.log(sectorA_Scanner === sectorB_Scanner);
 const scanner01 = sectorA_Scanner;
-
+console.log(scanner01 === sectorA_Scanner);
 // DEMO: Test if scanner01 is a clone/copy
 
 // DEMO: Make a git commit!
