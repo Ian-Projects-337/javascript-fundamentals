da110374d395bfe67e2e97d04c04626f3b908a3e
Ian <Ian.f.becker@gmail.com>
2026-05-26 18:47:30 -0500

    Finished video demo

--- tutorials/08-objects/object-methods/exercises/demo.js
+++ tutorials/08-objects/object-methods/exercises/demo.js
@@ -10,10 +10,11 @@ const defensiveDrone = {
 	modelId: "Aegis-04",
 	ammoCount: 50,
 	// DEMO: Assign the function by reference
+  activatePropulsion: fireThruster,
 };
 
 // DEMO: Invoke the method directly off the object structure
-
+defensiveDrone.activatePropulsion();
 /** EXAMPLE 2 - INLINE METHODS & THE 'THIS' KEYWORD **/
 
 const lifeSupportSystem = {
@@ -22,11 +23,24 @@ const lifeSupportSystem = {
 	criticalThreshold: 90,
 	// DEMO: Declare a method directly inline inside the object configuration
 	// DEMO: Declare a method that can modify sister properties
+  evaluateAtmosphere: function () {
+    if (this.oxygenPercentage < this.criticalThreshold) {
+      return `ALERT: ${this.sector} oxygen levels have dropped to ${this.oxygenPercentage}`
+    }else {
+      return `${this.sector} atospheric balance nominal at ${this.oxygenPercentage}`
+    }
+  },
+  injectPureOxyygen: function (tonnage) {
+    this.oxygenPercentage += tonnage;
+    console.log(`Injected ${tonnage} of oxygen mass into internal ducts.`)
+  }
 };
 
 // DEMO: Execute evaluation
-
+console.log(lifeSupportSystem.evaluateAtmosphere());
 // DEMO: Modify state via method behavior, then re-evaluate
+lifeSupportSystem.injectPureOxyygen(10);
+console.log(lifeSupportSystem.evaluateAtmosphere());
 
 // DEMO: Make a git commit!
 
