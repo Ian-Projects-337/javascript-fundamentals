b24714e4bcb25bac6321bc3c0534ee61eb9ce1f2
Ian <Ian.f.becker@gmail.com>
2026-05-26 07:50:30 -0500

    Finalized demo with added notes

--- tutorials/09-more-functions/higher-order-functions/exercises/demo.js
+++ tutorials/09-more-functions/higher-order-functions/exercises/demo.js
@@ -4,19 +4,28 @@
 
 // DEMO: Define a HOF that accepts a target name and a function that can describe
 // specifically how that target is engaged.
-
+function engageTacticalComputer(targetName, engageTarget) {
+  console.log(`RADAR TRACKING: Locking onto ${targetName}`);
+  console.log(`SYSTEM ENGAGED: ${engageTarget(targetName)}`);
+}
 // DEMO: Pass an in-place contractor to SCAN the target
-
+engageTacticalComputer("Unidentifed Frigate", target => `Scan of ${target} complete: Hull composition is 80% carbon`)
 // DEMO: Pass a completely different contractor to WEAPON LOCK the target
-
+engageTacticalComputer("Scout Drone -04", target => `Target locked. Railgun velocity calibrate to engage ${target}` );
 /** EXAMPLE 2 - RETURNING A FUNCTION FROM A HOF **/
 
 // DEMO: Define an HOF that can manufacture and return brand-new specialized functions
-
+function createThrusterrBurnCalculator(burnMultiplier) {
+  return basefuel => basefuel * burnMultiplier;
+}
 // DEMO: Use the HOF to manufacture two distinct calculation engines
-
+// setting the multiplier
+const getStandardBurn = createThrusterrBurnCalculator(1.5);
+const getEmergencyEscapeBurn = createThrusterrBurnCalculator(4.0);
 // DEMO: Call each manufactored function and log a report:
-
+// setting the basefuel 
+console.log(`Standard fuel required ${getStandardBurn(100)}t`);
+console.log(`Emergency fuel required ${getEmergencyEscapeBurn(100)}t`)
 // DEMO: Make a git commit!
 
 /* 
