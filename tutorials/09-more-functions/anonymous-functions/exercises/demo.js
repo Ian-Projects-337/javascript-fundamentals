d59d95de20d5fb394d0318260c350664c9159c69
Ian <Ian.f.becker@gmail.com>
2026-05-26 07:21:16 -0500

    Finished demo

--- tutorials/09-more-functions/anonymous-functions/exercises/demo.js
+++ tutorials/09-more-functions/anonymous-functions/exercises/demo.js
@@ -1,4 +1,4 @@
-/**** ANONYMOUS FUNCTIONS ****/
+ /**** ANONYMOUS FUNCTIONS ****/
 
 /** EXAMPLE 1 - IMMEDIATE IN-PLACE EXECUTION **/
 
@@ -10,7 +10,7 @@ logScannerReport("Sector Delta");
 logScannerReport("Sector Alpha");
 
 // DEMO: Execute the same function once without naming it
-
+(sector => console.log(`Internal scanner pinged at ${sector}.`))("Sector Beta");
 /** EXAMPLE 2 - A SIMPLE IN-PLACE USE CASE **/
 
 const blastOff = () => console.log("🚀 Thrusters ignited! Capsule has cleared the launchpad.");
@@ -22,10 +22,11 @@ console.log("Initializing countdown sequence... 3... 2... 1...");
 // Argument 2: The delay time in milliseconds (3000ms = 3s).
 
 // DEMO Use setTimeout() for the full countdown and a truly anonymous, single-use function
-
+setTimeout(blastOff,5000);
 // DEMO: Make a git commit!
-
+setTimeout(() => console.log("🚀 We have lift off! Capsule has cleared the launchpad."),3000)
 /* 
     Follow up with additional exercises after demo for hands-on practice 
     with problem-solving and coding!
 */
+
